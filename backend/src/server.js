c370c6f27989c91c2c454f6c053fb0173417f98b
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-28 17:31:16 +0530

    Onboarding Endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import "dotenv/config";
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
