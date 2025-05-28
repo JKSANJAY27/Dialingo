ad663f1c461beb8499f6776188b7703a804f47a6
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-28 20:03:15 +0530

    Friend and Chat Routes Setup

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -19,5 +19,10 @@ export const upsertStreamUser = async (userData) => {
 };
 
 export const generateStreamToken = (userId) => {
-    
+    try {
+        const userIdStr = userId.toString();
+        return streamClient.createToken(userIdStr);
+    } catch(error) {
+        console.error("Error generating Stream token:", error);
+    }
 };
