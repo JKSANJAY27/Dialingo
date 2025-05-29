9270ac2eed8f71c51e59fe513832dbd5a3862f33
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-29 18:30:41 +0530

    Chat Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,12 @@ const App = () => {
           </Layout>
         ) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} /> )}/>
         <Route path='/call' element={isAuthenticated ? <CallPage /> : <Navigate to='/login' />} />
-        <Route path='/chat' element={isAuthenticated ? <ChatPage /> : <Navigate to='/login' />} />
+        <Route path='/chat/:id' element={isAuthenticated && isOnboarded ? 
+          (<Layout showSidebar={false}>
+            <ChatPage />
+          </Layout>
+          ) : <Navigate to={!isAuthenticated ? "/login" : "/onboarding" } />
+        } />
         <Route path='/onboarding' element={isAuthenticated ? (!isOnboarded ? (<OnboardingPage />) : (<Navigate to="/" />)) : (<Navigate to='/login' />)} />
       </Routes>
       <Toaster />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
-import { BellIcon, LogOutIcon } from "lucide-react";
+import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
 
