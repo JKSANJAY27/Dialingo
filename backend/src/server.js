6b2126ee567c8148ba19dce9db7e0648e7b57cf7
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-05-29 13:01:37 +0530

    Signup Page

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,15 @@ import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
